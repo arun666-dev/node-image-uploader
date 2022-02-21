31baceb36f08324c3677e3ff5022b0d4a3c8f650
arun <arun@test.com>
2022-02-21 17:16:52 +0530

    2

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.engine(
 
 // calling routes
 app.use("/", require("./server/router/router"));
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
-app.listen(5000, () =>
+app.listen(port, () =>
   console.log(`server is started on http://localhost:${port}`)
 );
